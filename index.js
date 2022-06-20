949fd79fa52dd778b1e6a651f8b05fe05b0b8348
unknown <vphuc.coder@gmail.com>
2022-06-20 11:13:50 +0700

    config for dev env connect to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
+mongoose
+  .connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+  )
+  .then(() => console.log("Successfully connect to DB"))
+  .catch((e) => console.log(e));
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hi There!!s!!</h2>");
+  res.send("<h2>Hi Thersse!!s!!</h2>");
 });
 
 const port = process.env.PORT || 3000;
